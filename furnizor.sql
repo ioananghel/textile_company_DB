9f386e7051e4eb55913bf2a4d0be838f81a00c45
Ioan Anghel <92737521+ioananghel@users.noreply.github.com>
2023-01-08 22:42:39 +0200

    Add files via upload

---
+++ furnizor.sql
@@ -0,0 +1,9 @@
+---INSERT INTO Furnizor values(id_furnizor, nume)
+
+INSERT INTO Furnizor values(0, 'Politext'); --- Poliester
+INSERT INTO Furnizor values(1, 'Adra Group'); --- Polipropilena laminata
+INSERT INTO Furnizor values(2, 'Popescu Conf'); --- Polietilena
+INSERT INTO Furnizor values(3, 'NeoTextiles'); --- Neopren
+INSERT INTO Furnizor values(4, 'Firesist'); --- Fibra poliacrilonitrilica termooxidata ignifuga
+INSERT INTO Furnizor values(5, 'Elastik'); --- Elastic
+INSERT INTO Furnizor values(6, 'Zips Conf'); --- Fermoare
---
+++ utilaje.sql
@@ -0,0 +1,25 @@
+---INSERT INTO Utilaj values(id_utilaj, id_departament, nume_utilaj, model_utilaj)
+
+INSERT INTO Utilaj values(0, 1, 'Masina de cusut', 'Juki DDL-9000B');
+INSERT INTO Utilaj values(1, 1, 'Cutter', 'Eastman T480X');
+INSERT INTO Utilaj values(2, 1, 'Masina de taiat etichete', 'Reliable 5000M');
+INSERT INTO Utilaj values(3, 1, 'Masina de ambalat', 'Kite Packaging TP-350');
+
+INSERT INTO Utilaj values(4, 2, 'Masina de cusut', 'Juki DDL-9000B');
+INSERT INTO Utilaj values(5, 2, 'Cutter', 'Eastman T480X');
+INSERT INTO Utilaj values(6, 2, 'Masina de taiat etichete', 'Reliable 5000M');
+INSERT INTO Utilaj values(7, 2, 'Masina de ambalat', 'Kite Packaging TP-350');
+
+INSERT INTO Utilaj values(8, 4, 'Masina de cusut', 'Juki DDL-9000B');
+INSERT INTO Utilaj values(9, 4, 'Cutter', 'Eastman T480X');
+INSERT INTO Utilaj values(10, 4, 'Masina de taiat etichete', 'Reliable 5000M');
+INSERT INTO Utilaj values(11, 4, 'Masina de ambalat', 'Kite Packaging TP-350');
+
+INSERT INTO Utilaj values(12, 3, 'Densitometru', 'Hach HI 93703');
+INSERT INTO Utilaj values(13, 3, 'pH-metre', 'Hach HI 8424');
+INSERT INTO Utilaj values(14, 3, 'Cantar de precizie', 'Mettler XPE1202');
+INSERT INTO Utilaj values(15, 3, 'Pipeta', 'Eppendorf Reference 2');
+INSERT INTO Utilaj values(16, 3, 'Agitator magnetic', 'IKA C-MAG');
+
+INSERT INTO Utilaj values(17, 5, 'Stivuitor', 'EFL181');
+INSERT INTO Utilaj values(18, 5, 'Transpalet', 'CDD12-A2MJ-SZ');
