509444521edd19d126ca2c69876957ab850a4a5e
Ioan Anghel <92737521+ioananghel@users.noreply.github.com>
2023-01-12 13:19:27 +0200

    Delete client.sql

--- client.sql
+++
@@ -1,7 +0,0 @@
----INSERT INTO Client values(id_client, id_obiect)
-
-INSERT INTO Client values(0, 0); ---Combinezon impermeabil de unica folosinta
-INSERT INTO Client values(1, 1); ---Combinezon categoria III
-INSERT INTO Client values(2, 3); ---Costum anticaloric aluminizat
-INSERT INTO Client values(3, 4); ---Manusi de protectie la temperaturi inalte
-INSERT INTO Client values(4, 6); ---Costum uscat scufundari
